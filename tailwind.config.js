7dcee995b9315aefef56630ef27b81a89e781e72
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2024-11-27 08:13:56 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js,php}',
+  ],
+  theme: {
+    colors: {
+      'noir': '#000000',
+      'blanc': '#ffffff',
+    },
+    fontFamily: {
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
